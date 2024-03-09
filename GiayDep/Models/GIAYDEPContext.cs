ef92d756eb767e8d7d3b87defe8b3603962baf67
hanh my <studyhanh@gmail.com>
2024-03-09 20:42:03 +0700

    DATA

--- GiayDep/Models/GIAYDEPContext.cs
+++ GiayDep/Models/GIAYDEPContext.cs
@@ -36,7 +36,7 @@ namespace GiayDep.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=TQS20052004\\SQLEXPRESS;Initial Catalog=GIAYDEP;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+                optionsBuilder.UseSqlServer("Data Source=MYHANH\\MYHANH;Initial Catalog=GIAYDEP;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
             }
         }
 
